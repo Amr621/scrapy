48634685793038a3cd89d900a88e7620461dba32
Amr621 <47938102+Amr621@users.noreply.github.com>
2019-02-28 15:08:37 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -18,7 +18,7 @@ vid_src = article.find('iframe', class_='youtube-player')['src']
 # print(vid_src)
 vid_id = vid_src.split('/')[4]
 vid_id = vid_id.split('?')[0]
-print(vid_id)
+# print(vid_id)
 
 yt_link = f'https://youtube.com/watch?v={vid_id}'
 print(yt_link)
